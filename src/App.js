c1ca1effd304bc297c590d92a8745b1ba893d7b0
Matthew Madden <mmadde768@gmail.com>
2017-06-20 16:45:12 -0500

    Moved api directory. Removed default React page. Added Header, Content area, and Footer Components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Footer from './Footer';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+  	render() {
+    	return (
+      	<div className="App">
+        	<Header />
+        	<div>
+				This is the Content Area
+			</div>
+        	<Footer />
+      	</div>
+    	);
+  	}
 }
 
 export default App;
