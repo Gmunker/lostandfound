2cef06291174257f2cfbd77fd13581e1b6a1c433
Matthew Madden <mmadde768@gmail.com>
2017-06-21 16:24:04 -0500

    Added id to json file and changed Header

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,14 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return(
-            <div>
-                This is the Header
+            <div className="mainHeader">
+                <nav className="mainNav">
+                    <ul>
+                        <li>
+                            <button>Add a new Animal</button>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         )
     }
