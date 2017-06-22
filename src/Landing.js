0b947ac81a7b09e9c1d5de237343e7c35c4f5c45
Matthew Madden <mmadde768@gmail.com>
2017-06-21 23:04:48 -0500

    Added Sass preprocessor. Landing page background and images.

--- src/Landing.js
+++ src/Landing.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import dogOne from './images/DogOne.jpg';
+import dogTwo from './images/DogTwo.jpg';
+import dogThree from './images/DogThree.jpg';
+import dogFour from './images/DogFour.jpg';
 
 const Landing = (props) => {
     return(
-        <div>
+        <div className="topContainer">
+            <div className="topContainer__dogImages">
+                <img className="topContainer__dogImages__imgage1" src={dogOne} alt=""/>
+                <img className="topContainer__dogImages__imgage2" src={dogTwo} alt=""/>
+                <img className="topContainer__dogImages__imgage3" src={dogThree} alt=""/>
+                <img className="topContainer__dogImages__imgage4" src={dogFour} alt=""/>
+            </div>
             <button onClick={props.switchPage} value="List">List</button>
             <button onClick={props.switchPage} value="Add">Add</button>
         </div>
