895403dec5ced534731cf09a9291c9ec582d209d
Matthew Madden <mmadde768@gmail.com>
2017-06-23 19:35:52 -0500

    Updated List to pull data from Firebase

--- src/List.js
+++ src/List.js
@@ -1,68 +1,54 @@
-import React, { Component } from 'react';
+import React from 'react';
 import myData from './temp_api/masterList.json'
 
+const Filter = (props) => {
+	return (
+		<div>
 
-class DogTable extends React.Component {
-  render() {
-    var rows = [];
-    this.props.dogs.forEach(function(dog) {
-     
-      rows.push(<ListRow dog={dog} key={dog.id} />);
-    });
-    return (
-        <body>
-      <table>
-        <thead>
-          <tr>
-            <th class="text-left">Date</th>
-            <th>Breed</th>
-            <th>Color</th>
-                        <th>Sex</th>
-
-          </tr>
-        </thead>
-        <tbody>{rows}</tbody>
-      </table>
-      </body>
-    );
-  }
+		</div>
+	)
 }
 
-class ListRow extends React.Component {
-  render() {
-   var missing = this.props.dog.found ?
-      this.props.dog.date :
-      <span style={{color: 'red'}}>
-        {this.props.dog.date}
-      </span>;
-    return (
-      <tr>
-        <td>{missing}</td>
-        <td>{this.props.dog.breed}</td>
-
-                <td>{this.props.dog.color}</td>
-
-                <td>{this.props.dog.sex}</td>
-      </tr>
-    );
-  }
+const DogTable = (props) => {
+	const Animals = props.Animals;
+	return (
+		<table>
+			<thead>
+				<tr>
+					<th>Location</th>
+					<th>Color</th>
+					<th>Breed</th>
+				</tr>
+			</thead>
+			<tbody>
+				{Animals.map((Animal) => {
+					return(
+						<tr key={Animal.Id}>
+							<td>{Animal.Location}</td>
+							<td>{Animal.Color}</td>
+							<td>{Animal.Breed}</td>
+						</tr>
+					)
+				})}
+			</tbody>
+		</table>
+	)
 }
 
-const List = (props) => {
-   
-        return(
-        <div className="listContent">
-            <div className = "dTable">
-                      <DogTable dogs={myData} />
-            </div>
-             <nav className="pageNavigation">
-                    <button onClick={props.switchPage} value="Landing">Home</button>
-                    </nav>
-        </div>
+class List extends React.Component {
+	
 
-            
-        );
-    }
+	
+   	render() {
+		return(
+			<div className="listContent">
+				<Filter />
+				<DogTable dogs={myData} Animals={this.props.Animals} />
+				<button>Add New Animal</button>
+			</div>
+		)
+   }
+}
 
 
 
