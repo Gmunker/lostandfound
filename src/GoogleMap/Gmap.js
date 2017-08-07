1559bc276d40af2e80fa9cc4391e3661f11b6013
Greg Munker <gmunker@gmail.com>
2017-08-07 00:31:30 -0700

    minor changes

--- src/GoogleMap/Gmap.js
+++ src/GoogleMap/Gmap.js
@@ -2,41 +2,62 @@ import React, { Component } from 'react';
 
 const google = window.google;
 
-
-  
-
-class Gmap extends Component {
-
-  componentDidMount() {
-    let Donelson = {
+let Donelson = {
       lat: 36.170295,
       lng: -86.674846
-    };
-    let map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 15,
-      center: Donelson
-    });
+    }
+
+class Gmap extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      markers: [{
+          position: {
+              lat: 36.1571445511326,
+              lng: -86.67903900146484
+          },
+          key: 'Spot',
+          defaultAnimation: 2
+      }]
+    }
   }
 
-  handleClick(event) {
-    event.persist()
-    // let marker = new google.maps.Marker({
-    //   position: {
-    //     lat: e.latLng.lat(),
-    //     lng: e.latLng.lng()
-    //   }
-    // })
-    console.log(event)
+  componentDidMount() {  
+
+    let map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 14,
+      center: Donelson
+    });
+
+    let markers = this.state.markers.map(loc => {
+      return new google.maps.Marker({
+        position: loc.position,
+        map: map
+      })
+    })
+
+    map.addListener('click', function(e) {
+      placeMarkerAndPanTo(e.latLng, map);
+      console.log(e.latLng.lat())
+      console.log(e.latLng.lng())
+    });
+
+    function placeMarkerAndPanTo(latLng, map) {
+      var marker = new google.maps.Marker({
+        position: latLng,
+        map: map
+      });
+      map.panTo(latLng);
+    }
   }
   
   render() {
-    console.log(this.map)
     return (
       <div>
         <div 
           id="map" 
           style={{height: "100vh", width: "100vw"}}
-          onClick={this.handleClick.bind(this)}
         ></div>        
       </div>
     )
