cd4d2f97aec71eab15cf4a5cf59ea81b92af4d52
Matthew Madden <mmadde768@gmail.com>
2017-08-14 16:23:22 -0500

    Removed unnecessary files. Writing and reading from store based on map clicks. Updated sampleAnimal for reference.

--- src/GoogleMap/AddGmap.js
+++ src/GoogleMap/AddGmap.js
@@ -1,59 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import regions from './geojson.json';
+import { animalInfo } from '../actions/animalActions';
 const baseUrl = 'https://raw.githubusercontent.com/m-madden/lostandfound/master/';
-
-let dummyData = [{
-        Name: "Spot",
-        Color: "White",
-        Breed: "Great Dame",
-        Date: new Date().toString(),
-        Location: {
-          position: [{
-            lat: 36.5571445511326,
-            lng: -86.17903900146484
-          }],
-          key: this.name,
-          defaultAnimation: 2
-        },
-        Type: "Dog",
-        Status: ["Found"]
-      },{
-        Name: "Whiskers",
-        Color: "Orange",
-        Breed: "Evil",
-        Date: new Date().toString(),
-        Location: {
-          position: [{
-            lat: 36.0571445511326,
-            lng: -86.97903900146484
-          }],
-          key: this.name,
-          defaultAnimation: 2
-        },
-        Type: "Cat",
-        Status: ["Lost"]
-      },{
-        Name: "Rusty",
-        Color: "White with Black",
-        Breed: "Jack Russel",
-        Date: new Date().toString(),
-        Location: {
-          position: [{
-            lat: 36.9121445511326,
-            lng: -85.17903900146484
-          },
-          {
-            lat: 36.9121445511396,
-            lng: -85.77903900141484
-          }],
-          key: this.name,
-          defaultAnimation: 2
-        },
-        Type: "Dog",
-        Status: ["Lost", "Found"]
-      }
-  ]
 
 var marker
 
@@ -80,6 +29,16 @@ class AddGmap extends Component {
 		icon: baseUrl + this.props.newAnimal.Status + this.props.newAnimal.Type + "Icon.png"
 		});
     map.panTo(latLng);
+    let regionName = this.findRegion(latLng);
+    let location = {
+      lat: latLng.lat(),
+      lng: latLng.lng(),
+      region: regionName
+    }
+    this.props.dispatch(animalInfo({
+      ...this.props.newAnimal,
+      location
+    }))
   }
   
   findRegion(latLng) {
@@ -91,10 +50,11 @@ class AddGmap extends Component {
       }
     }
     if(regionName) {
-      console.log(regionName)
+      
     } else {
-      console.log('Outside defined regions')
+      regionName = "Outside defined area"
     }
+    return regionName
   }
 	
 	componentWillReceiveProps(props) {
@@ -104,82 +64,9 @@ class AddGmap extends Component {
 			this.placeMarkerAndPanTo(e.latLng, map);
 		}.bind(this));
 	}
-	
-	// componentWillReceiveProps(props) {
-	// 	let map = new props.google.maps.Map(this.refs.map, {
-	// 		zoom: 11,
-	// 		center: {
-	// 			lat: 36.170295,
-	// 			lng: -86.674846
-	// 		}
-	// 	});
-	// }
-
-  // componentWillReceiveProps ({ isScriptLoaded, isScriptLoadSucceed }) {
-  //   if (isScriptLoaded && !this.props.isScriptLoaded) { // load finished
-  //     if (isScriptLoadSucceed) {
-  //       let google = window.google
-        
-  //       let map = new google.maps.Map(this.refs.map, {
-  //         zoom: 11,
-  //         center: {
-  //           lat: 36.170295,
-  //           lng: -86.674846
-  //         }
-  //       });
-
-  //     let markers = this.state.dummyData.map(animal => {
-  //       animal.Location.position.map((position, index) => {
-  //         return new google.maps.Marker({
-  //           position: position,
-  //           map: map,
-  //           icon: pullIcon(animal, index)
-  //         })
-  //       })
-  //     });
-
-  //     map.addListener('click', function(e) {
-  //       findRegion(e.latLng);
-  //       placeMarkerAndPanTo(e.latLng, map);
-  //     });
-
-  //     function findRegion(latLng) {
-  //       var regionName;
-  //       for(var i=0; i<regions.length;i++) {
-  //         var currentPoly = new google.maps.Polygon({paths: regions[i].polygon});
-  //         if(google.maps.geometry.poly.containsLocation(latLng, currentPoly)) {
-  //           regionName = regions[i].name;
-  //         }
-  //       }
-  //       if(regionName) {
-  //         map.region = regionName
-  //       } else {
-  //         sessionStorage.setItem('regionName', 'Outside defined regions');
-  //       }
-  //     }
-
-  //     function placeMarkerAndPanTo(latLng, map) {
-  //       var marker = new google.maps.Marker({
-  //         position: latLng,
-  //         map,
-  //       });
-  //       map.panTo(latLng);
-  //     }
-
-  //     function pullIcon(animal, index) {
-  //       var status = animal.Status[index] === "Found" ? "found" : "lost";
-  //       var type = animal.Type === "Cat" ? "Cat" : "Dog";
-  //       return  baseUrl + status + type + "Icon.png"
-  //     }
-  //   }
-  // }
-  // // else this.props.onError()
-  // }
   
   render() {
-	  
     return (
-      
       <div className="mapContainer" onClick={this.placeMarkerAndPanTo}>
         <div id="map"></div>
       </div>
