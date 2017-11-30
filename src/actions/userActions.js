f0e2e7b77d712bf641ac1233e3fc687d946b519b
Greg Munker <gmunker@gmail.com>
2017-11-30 13:16:13 -0700

    removed consolelogs

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,51 +1,48 @@
-import firebase from '../firebase';
+import firebase from "../firebase"
 
 export function login(email, password) {
-
-  return function(dispatch) {
-    firebase.auth().signInWithEmailAndPassword(email, password)
-    .then(result => {
-<<<<<<< HEAD
-      console.log(result)
-      dispatch({type: "USER_LOGIN", payload: result.uid})
-      console.log("User Logged In!")
-=======
-      dispatch({type: "USER_LOGIN", payload: result.uid})     
->>>>>>> e079b49070b2e3ef32dc40e32bdc894a6bbafe7f
-    })
-    .catch(error => {
-      let errorCode = error.code;
-      let errorMessage = error.message;
-      if (errorCode === 'auth/wrong-password') {
-        alert('Wrong password.');
-      } else {
-        alert(errorMessage);
-      }
-      console.log(error);
-    });
-  }
+	return function(dispatch) {
+		firebase
+			.auth()
+			.signInWithEmailAndPassword(email, password)
+			.then(result => {
+				dispatch({ type: "USER_LOGIN", payload: result.uid })
+			})
+			.catch(error => {
+				let errorCode = error.code
+				let errorMessage = error.message
+				if (errorCode === "auth/wrong-password") {
+					alert("Wrong password.")
+				} else {
+					alert(errorMessage)
+				}
+				console.log(error)
+			})
+	}
 }
 
 export function checkAuth() {
-  return function(dispatch) {
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        console.log("Active User")
-        dispatch({type: "USER_LOGIN", payload: user.uid})
-      } else {
-        console.log("No Active User")
-        return null
-      }
-    });
-  }
+	return function(dispatch) {
+		firebase.auth().onAuthStateChanged(function(user) {
+			if (user) {
+				console.log("Active User")
+				dispatch({ type: "USER_LOGIN", payload: user.uid })
+			} else {
+				console.log("No Active User")
+				return null
+			}
+		})
+	}
 }
 
 export function logout() {
-  return function(dispatch) {
-    firebase.auth().signOut()
-      .then(result => {
-        dispatch({type: "USER_LOGOUT"})
-        console.log("User Logged Out!");
-      })
-  }
+	return function(dispatch) {
+		firebase
+			.auth()
+			.signOut()
+			.then(result => {
+				dispatch({ type: "USER_LOGOUT" })
+				console.log("User Logged Out!")
+			})
+	}
 }
